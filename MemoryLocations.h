ee630c830107288b653933cf883788fac9ade94d
swsheep <wong.369@osu.edu>
2014-06-16 17:28:25 -0400

    Updated to include MAC address

--- MemoryLocations.h
+++ MemoryLocations.h
@@ -2,14 +2,15 @@
 #define __AQE_MEMORY_LOCATIONS__
 
 #define MCUSR_ADDRESS 0 //one byte to save value of MCUSR at startup
-#define tinyWDT_STATUS	1 //one byte for status info
-#define ACTIVATION_STATUS_EEPROM_ADDRESS 2  // one byte with a magic value
-#define API_KEY_EEPROM_ADDRESS           3  // API KEY IS 48 characters + 1 for null terminator = 49 bytes
-#define FEED_ID_EEPROM_ADDRESS           52 // FEED ID IS (at most) 9 characters + 1 for null terminator = 10 bytes
-#define CAL_MAGIC	64 //4 bytes for long
-#define CO_cal	68	//4 bytes for each float
-#define NO2_cal	72
-#define O3_cal	76
+#define MAC_ADDRESS  1 //6 bytes
+#define tinyWDT_STATUS	7 //one byte for status info
+#define ACTIVATION_STATUS_EEPROM_ADDRESS 8  // one byte with a magic value
+#define API_KEY_EEPROM_ADDRESS           9  // API KEY IS 48 characters + 1 for null terminator = 49 bytes
+#define FEED_ID_EEPROM_ADDRESS           58 // FEED ID IS (at most) 9 characters + 1 for null terminator = 10 bytes
+#define CAL_MAGIC	70 //4 bytes for long
+#define CO_cal	74	//4 bytes for each float
+#define NO2_cal	78
+#define O3_cal	82
 
 
 
